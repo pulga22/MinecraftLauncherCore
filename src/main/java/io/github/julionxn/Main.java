9c1d602bd33d10f7bd382cb555181d32e211688d
julionxn <soyelmerojulion@gmail.com>
2025-03-17 10:33:40 -0600

    fix: main launching with url fetching

--- src/main/java/io/github/julionxn/Main.java
+++ src/main/java/io/github/julionxn/Main.java
@@ -1,12 +1,22 @@
 package io.github.julionxn;
 
+import io.github.julionxn.instance.MinecraftInstance;
+import io.github.julionxn.instance.MinecraftOptions;
+import io.github.julionxn.instance.PlayerInfo;
+import io.github.julionxn.profile.Profile;
+import io.github.julionxn.profile.URLProfile;
+import io.github.julionxn.profile.URLProfiles;
+import io.github.julionxn.version.MinecraftVersion;
+
+import java.util.Optional;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        /* ==================== WITH URL FETCHING ======================
+    public static void main(String[] args) {
+        //==================== WITH URL FETCHING ======================
         LauncherData launcherData = new LauncherData("JNLauncher", "1.0", "C:/");
         Launcher launcher = new Launcher(launcherData);
         launcher.start();
-        URLProfiles profiles = launcher.getProfilesController().getProfilesFrom("http://localhost/Testing/");
+        URLProfiles profiles = launcher.getProfilesController().getValidProfilesFrom("http://localhost/Testing/", "xd");
         URLProfile urlProfile = profiles.getAllProfiles().get(0);
         Optional<Profile> profileOpt = urlProfile.tempProfile().save();
         if (profileOpt.isEmpty()) return;
@@ -18,7 +28,7 @@ public class Main {
         });
         MinecraftInstance instance = new MinecraftInstance(launcher, version, minecraftOptions, profile, new PlayerInfo("pepe", "xd", "a"));
         instance.run();
-         */
+
 
         /*
         // ==================== WITHOUT URL FETCHING ======================
